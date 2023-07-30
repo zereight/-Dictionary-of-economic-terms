22faa7ca00885f01f76c842305ea79886e84095f
simple <simple@woowahan.com>
2023-07-30 13:57:49 +0900

    ios 스플래시 + 앱아이콘 변경

--- src/components/Brand/Brand.tsx
+++ src/components/Brand/Brand.tsx
@@ -1,32 +1,20 @@
 import React from 'react';
-import { View, Image } from 'react-native';
-import { useTheme } from '../../hooks';
+import { Text, View } from 'react-native';
 
-type Props = {
-  height?: number | string;
-  width?: number | string;
-  mode?: 'contain' | 'cover' | 'stretch' | 'repeat' | 'center';
-};
-
-const Brand = ({ height, width, mode }: Props) => {
-  const { Layout, Images } = useTheme();
-
+const Brand = () => {
   return (
-    <View testID={'brand-img-wrapper'} style={{ height, width }}>
-      <Image
-        testID={'brand-img'}
-        style={Layout.fullSize}
-        source={Images.logo}
-        resizeMode={mode}
-      />
+    <View testID={'brand-img-wrapper'}>
+      <Text
+        style={{
+          fontSize: 30,
+          fontWeight: 'bold',
+          textAlign: 'center',
+        }}
+      >
+        {'부자가 될 준비 되셨나요?'}
+      </Text>
     </View>
   );
-};
-
-Brand.defaultProps = {
-  height: 200,
-  width: 200,
-  mode: 'contain',
 };
 
 export default Brand;
